b9440f563f12bb6fe90b88abe2c3807a45420dd4
jaiswalatul <jaiswalatul018@gmail.com>
2023-11-11 16:16:28 +0530

    datatypes

---
+++ 01variable.js
@@ -0,0 +1,13 @@
+console.log("hindi buddy hii");
+const aj=14453
+let aja="atul@123"
+var abc="12345"
+def="mirzapur";
+
+// aj=345 const change not allowed
+//baki all can changes
+
+console.log(aja,aj,abc,def)
+//new format of printing
+console.table([aj,aja,abc,def])
+
---
+++ 02 dattypes.js
@@ -0,0 +1,21 @@
+"use strict"  //treat all js code as newer version
+//alert (3+3) we are using nodejs not browswer
+
+//DATA TYPES
+// number=> 2t0 power53
+//big int 
+//string=""
+//boolean=>t/f
+
+//null=>standalone value
+//ex- let no.of f in apple=null
+
+//undefined =>
+//ex- let state
+
+//symbols=>mostly used for uniqness 
+let age=20
+console.log(typeof "atul") //string
+console.log(typeof age) //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
